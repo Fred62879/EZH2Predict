7702db5b2b0d6f7544efb9dca81ed5e3443aec5f
fred62879 <zhuotingxie-fred@outlook.com>
2020-03-27 18:38:29 -0700

    add public

---
+++ public/javascript/main.js
@@ -0,0 +1,8 @@
+$("button").click(function() {
+    alert("hello");
+    console.log("aa");
+});
+
+$("button").on("click", function() {
+    console.log("bb");
+});
---
+++ public/javascript/test.js
@@ -0,0 +1,8 @@
+// load("child_process").then((qs) => {
+//     console.log("succeed");
+// }).catch((err) => {
+//     console.log("fail");
+// });
+
+var child = require("child_process");
+console.log(1);
