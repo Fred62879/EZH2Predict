7167bae2fb04d00d4a204c6d825788dde5e73244
fred62879 <zhuotingxie-fred@outlook.com>
2020-03-27 19:03:06 -0700

    app debuged

--- app.js
+++ app.js
@@ -66,4 +66,4 @@ app.post('/ml', (req, res) => {
     res.redirect('/ml');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
