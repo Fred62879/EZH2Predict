eb6cb7de642b8f47eb62ffc59011ceb1ae4a3f34
fred62879 <zhuotingxie-fred@outlook.com>
2020-03-29 18:13:40 -0700

    set dir for r script

--- ml.R
+++ ml.R
@@ -33,3 +33,30 @@ subsummary<-list(pred,result,accuracy)
 # library(randomForest)
 suppressMessages(suppressWarnings(require(randomForest)))
 require(plyr,quietly=T)
+
+
+# split into 10 sets
+for (i in 1:10) {
+    result[[i]]<-ddply(dtpid, .(Mut), function(., seed) {
+        set.seed(seed); .[sample(1:nrow(.), trunc(nrow(.) * .1)),]
+    }
+    ,seed = i)
+    rownames(result[[i]])<-result[[i]]$ID
+    subsummary[[2]][[i]]<-result[[i]][,-1]    
+}
+
+# training, 1 time
+train<-dtlim1[!row.names(dtlim1)%in%rownames(result[[1]]),]
+
+#rf filtering
+rf <- randomForest(Mut~.,train,importance=T)
+imp <- importance(rf)
+imp <- imp[,ncol(imp)-1]
+rf.genes <- names(imp)[order(imp,decreasing=T)[1:20]]
+train<-train[,c("Mut",rf.genes)]
+
+model<-randomForest(Mut~.,train)
+subsummary[[1]][[1]]<-predict(model,result[[1]])
+subsummary[[3]][[1]]<-sum(ifelse(subsummary[[1]][[1]]==result[[1]]$Mut,100,0))/nrow(result[[1]])
+
+print(subsummary[[3]])
